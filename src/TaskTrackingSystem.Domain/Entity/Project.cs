0bda84af914fb68354673c56a2d43bcd67982a2e
HP <HP@DESKTOP-E8M3LD3>
2025-04-23 13:04:34 +0545

    Add DTOs folder and enhance Project entity
    Updated `TaskTrackingSystem.Application.Contracts.csproj` to include a new `DTOs` folder.
    Enhanced the `Project` class in `Project.cs` by adding `Id` and `UserId` properties, establishing a foreign key relationship with `IdentityUser`, and including necessary data annotations.

--- src/TaskTrackingSystem.Domain/Entity/Project.cs
+++ src/TaskTrackingSystem.Domain/Entity/Project.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Identity;
 
 namespace TaskTrackingSystem.Entity
 {
     public class Project : FullAuditedAggregateRoot<Guid>
     {
+        [Key]
+        public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public Guid UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; } // Use IdentityUser from Volo.Abp.Identity
 
         public ICollection<TaskItem> Tasks { get; set; }
     }
